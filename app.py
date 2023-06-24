e590b682db14c5ab314f3022e786dc3d1e60958f
610engineer <m_muto@jour-1.com>
2023-06-24 22:41:54 +0900

    チケット情報にチャット内容を追加してelasticsearchへ挿入

--- app.py
+++ app.py
@@ -21,9 +21,31 @@ def main():
 
     tickets = zenpy_client.tickets()
     for ticket in tickets:
+        # チケット情報をdict型に変換
         ticket_data = ticket.to_dict()
+
+        
+
+        comment_list = []
+        for comment in zenpy_client.tickets.comments(ticket = ticket.id):
+            comment_data = comment.to_dict()
+            comment_json = json.dumps(comment_data, indent = 4, ensure_ascii=False)
+            comment_list.append(comment_json)
+
+        #print(comment_list)
+
+        chat_json = {"chats": comment_list}
+
+        ticket_data.update(chat_json)
+        # チケット情報をjsonに変換
         ticket_json = json.dumps(ticket_data, indent = 4, ensure_ascii=False)
-        print("TICKET_JSON : ", ticket_json)
+        print(ticket_json)
+        
+
+        #print(comment_list)
+
+        
+        # print("TICKET_JSON : ", ticket_json)
         insert_elasticsearch(ticket_json)
 
     #for comment in zenpy_client.tickets.comments(ticket=3):
