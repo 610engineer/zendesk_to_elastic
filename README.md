8ec59390851ba996282c1431782269823440e575
610engineer <m_muto@jour-1.com>
2023-06-26 23:29:15 +0900

    added comment

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # zendesk_to_elastic
 
+# 前提条件
+Zendesk APIトークンを取得していること
+elasticsearchのDockerが起動していること
+pythonがインストールされていること
+開発時点では3.9
+
+以降のバージョンでは動作未確認
+
 # 環仮想環境境立ち上げ
 プロジェクトがあるディレクトリでターミナルを開き、以下のコマンド
 python -m venv venv
@@ -13,3 +21,6 @@ pip install -r requirements.txt
 
 # 環境変数設定
 env_sample.pyをコピーし、env.pyにrenameしたファイルに環境変数を設定する
+
+# 使用方法
+venv環境でapp.pyを起動させる</br>
--- app.py
+++ app.py
@@ -24,47 +24,44 @@ def main():
         # チケット情報をdict型に変換
         ticket_data = ticket.to_dict()
 
-        
-
         comment_list = []
+
+        # チケットIDからチャット履歴を取得
         for comment in zenpy_client.tickets.comments(ticket = ticket.id):
+
+            # チャットデータをdict型へ変換
             comment_data = comment.to_dict()
+
+            # dict型からjsonへ変換
             comment_json = json.dumps(comment_data, indent = 4, ensure_ascii=False)
+
+            # チャットリストへ追加
             comment_list.append(comment_json)
 
-        #print(comment_list)
-
+        # チャットリストをdict型に変換
         chat_json = {"chats": comment_list}
 
+        # チケット情報のdictにチャットリストを追加
         ticket_data.update(chat_json)
+
         # チケット情報をjsonに変換
         ticket_json = json.dumps(ticket_data, indent = 4, ensure_ascii=False)
-        print(ticket_json)
+        # print(ticket_json)
         
-
-        #print(comment_list)
-
-        
-        # print("TICKET_JSON : ", ticket_json)
+        # Elasticsearchへチケット情報を登録
         insert_elasticsearch(ticket_json)
 
-    #for comment in zenpy_client.tickets.comments(ticket=3):
-    #    print(comment.body)
-
-
 def insert_elasticsearch(json_data):
+
+    # エンドポイントを設定
     es = Elasticsearch(os.environ.get('ELASTICSEARCH_URL'))
 
+    # "zen_test"のインデックスを作成し、jsonを登録
     res = es.index(index = "zen_test",
                    document = json_data)
-    print(res)
-
+    
+    # エンドポイントとの接続をクローズ
     es.close()
-    
-    
-    
-
-
 
 if __name__ == "__main__":
     main()
